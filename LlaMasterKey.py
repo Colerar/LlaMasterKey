62ea2411212aec23ff3ef4a876e1d6918daff448
yazawazi <47273265+Yazawazi@users.noreply.github.com>
2023-12-26 00:39:23 +0800

    feat: support hugging face

--- LlaMasterKey.py
+++ LlaMasterKey.py
@@ -13,6 +13,7 @@ class Config:
     base_url: str
     openai_api_key: Optional[str] = None
     cohere_api_key: Optional[str] = None
+    hf_api_key: Optional[str] = None
 
     def __init__(self):
         _config: dict = {}
@@ -42,6 +43,12 @@ class Config:
             cohere_api_key = _config.get("CO_API_KEY")
         self.cohere_api_key = cohere_api_key
 
+        hf_api_key = os.environ.get("HF_API_KEY")
+        if hf_api_key is None:
+            hf_api_key = _config.get("HF_API_KEY")
+        self.hf_api_key = hf_api_key
+
+
 
 def generate_env(_dict: dict[str, str]) -> str:
     """
@@ -63,6 +70,9 @@ if config.openai_api_key:
 if config.cohere_api_key:
     user_env["CO_API_URL"] = config.base_url
     user_env["CO_API_KEY"] = "cohere"
+if config.hf_api_key:
+    user_env["HF_ENDPOINT"] = config.base_url + "/hf"
+    user_env["HF_API_KEY"] = "hugging_face"
 
 with open("generated-keys.env", "w") as f:
     f.write(generate_env(user_env))
@@ -73,7 +83,29 @@ app = FastAPI()
 client = httpx.AsyncClient()
 
 
-@app.api_route("/{path:path}", methods=["GET", "POST"])
+@app.api_route("/hf/{path:path}", methods=["GET", "POST", "HEAD"])
+async def hf_catch_all(request: Request, path: str, response: Response):
+    print(request.headers)
+    authorization = request.headers.get("authorization")
+    if authorization is None:
+        return await __reverse_proxy(request, "https://huggingface.co", None, replace_path="/hf")
+
+    split = authorization.split(' ')
+
+    if len(split) != 2:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return response
+
+    auth_type, auth_value = split
+
+    if auth_value != "hugging_face":
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return response
+
+    return await __reverse_proxy(request, "https://huggingface.co", config.hf_api_key, replace_path="/hf")
+
+
+@app.api_route("/{path:path}", methods=["GET", "POST", "HEAD"])
 async def catch_all(request: Request, path: str, response: Response):
     authorization = request.headers.get("authorization")
     if authorization is None:
@@ -99,13 +131,17 @@ async def catch_all(request: Request, path: str, response: Response):
             return response
 
 
-async def __reverse_proxy(request: Request, new_url: str, bearer_key: str):
+async def __reverse_proxy(request: Request, new_url: str, bearer_key: Optional[str], replace_path: Optional[str] = None):
     parsed_url = urlparse(new_url)
-    url = httpx.URL(url=f"{parsed_url.scheme}://{parsed_url.netloc}", path=parsed_url.path + request.url.path,
+    new_path = request.url.path
+    if replace_path is not None:
+        new_path = new_path.replace(replace_path, "")
+    url = httpx.URL(url=f"{parsed_url.scheme}://{parsed_url.netloc}", path=parsed_url.path + new_path,
                     query=request.url.query.encode("utf-8"))
     headers = request.headers.mutablecopy()
     headers["host"] = parsed_url.netloc
-    headers["authorization"] = f"Bearer {bearer_key}"
+    if bearer_key is not None:
+        headers["authorization"] = f"Bearer {bearer_key}"
 
     rp_req = client.build_request(request.method, url,
                                   headers=headers.raw,
